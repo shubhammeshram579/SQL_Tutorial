db3ea57f33c7689a7afd1899b45db6295762b19f
Shubham Meshram <105120972+shubhammeshram579@users.noreply.github.com>
2023-08-28 13:29:30 +0530

    Update Create_Table.sql

--- Create_Table.sql
+++ Create_Table.sql
@@ -9,10 +9,10 @@ USE sql_turial
 CREATE TABLE customer (
                         customer_id NUMERIC,
                         first_name VARCHAR(55),
-			            last_name VARCHAR(55),
-			            city VARCHAR(15),
-			            pin_code NUMERIC,
-			            product_name VARCHAR(250),
+			last_name VARCHAR(55),
+			city VARCHAR(15),
+			pin_code NUMERIC,
+			product_name VARCHAR(250),
                         product_order NUMERIC,
                         Sales NUMERIC
                        )
@@ -22,7 +22,7 @@ CREATE TABLE customer (
 /*insert into fuction is help to add new data points*/
 INSERT INTO customer()
 VALUES  (1,"shubham","meshram","Gondia",411601,"mobile",2,24000),
-	    (2,"Labham","Mesra","Nagpur",411001,"iphone",3,36000),
+	(2,"Labham","Mesra","Nagpur",411001,"iphone",3,36000),
         (3,"Aachal","varma","murpar",411010,"moto",4,43000),
         (4,"Pallavi","meshram","Hydrabad",411011,"realmy",7,12000),
         (5,"Parmanand","Agrval","Delhi",411012,"nokia",2,25000)
